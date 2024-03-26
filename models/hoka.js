deafc913ffe0e667d6768acd902edc0fef330593
Youjun Seo <76039485+SEO-YJ@users.noreply.github.com>
2024-03-27 08:48:32 +0900

    Feat: node-cron으로 토큰 발급 후, 실시간 데이터 가져오기 기능 추가

--- models/hoka.js
+++ models/hoka.js
@@ -1,55 +1,55 @@
-var XMLHttpRequest = require("xhr2");
-require("dotenv").config(); // 모듈 불러오기
-var {fetchStockPricesPeriodically_main1,fetchStockPricesPeriodically_main2, fetchStockPricesPeriodically_main3, fetchStockPricesPeriodically_sub1,fetchStockPricesPeriodically_sub2,fetchStockPricesPeriodically_sub3,fetchStockPricesPeriodically_sub4,fetchStockPricesPeriodically_sub5} = require("./gagonhoka")
-var count_item = 0;
-var getKOSPI200 = require("./queries/kospi200");
-var getKOSDAQ130 = require("./queries/kospi200");
-const pool = require("./dbConnect");
+// var XMLHttpRequest = require("xhr2");
+// require("dotenv").config(); // 모듈 불러오기
+// var {fetchStockPricesPeriodically_main1,fetchStockPricesPeriodically_main2, fetchStockPricesPeriodically_main3, fetchStockPricesPeriodically_sub1,fetchStockPricesPeriodically_sub2,fetchStockPricesPeriodically_sub3,fetchStockPricesPeriodically_sub4,fetchStockPricesPeriodically_sub5} = require("./gagonhoka")
+// var count_item = 0;
+// var getKOSPI200 = require("./queries/kospi200");
+// var getKOSDAQ130 = require("./queries/kospi200");
+// const pool = require("./dbConnect");
 
-// TEST
-// 한투 권장: 50, 200
-// 1. 30, 150
-// 성공: 10초 동안 225개
-// 실패: 10초 동안 5개
+// // TEST
+// // 한투 권장: 50, 200
+// // 1. 30, 150
+// // 성공: 10초 동안 225개
+// // 실패: 10초 동안 5개
 
-// 2. 30, 160
-// 성공: 10초 동안 223개
-// 실패: 10초 동안 3개
+// // 2. 30, 160
+// // 성공: 10초 동안 223개
+// // 실패: 10초 동안 3개
 
-pool.getConnection((err, conn) => {
-  if (err) {
-    console.error("DB Disconnected:", err);
-    return;
-  }
+// pool.getConnection((err, conn) => {
+//   if (err) {
+//     console.error("DB Disconnected:", err);
+//     return;
+//   }
 
-  // getKOSPI200 쿼리 실행
-  conn.query(getKOSPI200.getKOSPI200, (err, results) => {
-    if (err) {
-      console.log("Query Error:", err);
-      return;
-    }
+//   // getKOSPI200 쿼리 실행
+//   conn.query(getKOSPI200.getKOSPI200, (err, results) => {
+//     if (err) {
+//       console.log("Query Error:", err);
+//       return;
+//     }
 
-    const kospi200Codes = results.map((item) => item.stock_code);
-    // console.log("KOSPI 200 종목코드:", kospi200Codes);
+//     const kospi200Codes = results.map((item) => item.stock_code);
+//     // console.log("KOSPI 200 종목코드:", kospi200Codes);
 
-    // getKOSDAQ130 쿼리 실행
-    conn.query(getKOSDAQ130.getKOSDAQ130, (err, results) => {
-      if (err) {
-        console.log("Query Error:", err);
-        return;
-      }
+//     // getKOSDAQ130 쿼리 실행
+//     conn.query(getKOSDAQ130.getKOSDAQ130, (err, results) => {
+//       if (err) {
+//         console.log("Query Error:", err);
+//         return;
+//       }
 
-      const kosdaq200 = results.map((item) => item.stock_code);
-      // console.log("KOSDAQ 200 종목코드:", kosdaq200);
+//       const kosdaq200 = results.map((item) => item.stock_code);
+//       // console.log("KOSDAQ 200 종목코드:", kosdaq200);
 
-      fetchStockPricesPeriodically_main1(kospi200Codes);
-      fetchStockPricesPeriodically_main2(kospi200Codes);
-      fetchStockPricesPeriodically_main3(kospi200Codes);
-      fetchStockPricesPeriodically_sub1(kosdaq200);
-      fetchStockPricesPeriodically_sub2(kospi200Codes);
-      fetchStockPricesPeriodically_sub3(kospi200Codes);
-      fetchStockPricesPeriodically_sub4(kospi200Codes);
-      fetchStockPricesPeriodically_sub5(kospi200Codes);
-    });
-  });
-});
+//       fetchStockPricesPeriodically_main1(kospi200Codes);
+//       fetchStockPricesPeriodically_main2(kospi200Codes);
+//       fetchStockPricesPeriodically_main3(kospi200Codes);
+//       fetchStockPricesPeriodically_sub1(kosdaq200);
+//       fetchStockPricesPeriodically_sub2(kospi200Codes);
+//       fetchStockPricesPeriodically_sub3(kospi200Codes);
+//       fetchStockPricesPeriodically_sub4(kospi200Codes);
+//       fetchStockPricesPeriodically_sub5(kospi200Codes);
+//     });
+//   });
+// });
--- models/redisset.js
+++ models/redisset.js
@@ -1,29 +1,51 @@
-const publisher = require('./redis')
+const publisher = require("./redis");
 
 // 데이터 저장 및 변경 사항 알림
-function publish(stockCode, stockData){
+function publish(stockCode, stockData) {
   const channelName = `stockUpdates:${stockCode}`;
   publisher.set(stockCode, JSON.stringify(stockData), (err, reply) => {
     if (err) {
-      console.error('Error setting value in Redis:', err);
+      console.error("Error setting value in Redis:", err);
       return;
     }
-    console.log('Set data reply:', reply);
+    console.log("Set data reply:", reply);
     // 데이터 변경 사항을 해당 주식 코드의 채널에 알림
-    publisher.publish(channelName, JSON.stringify({ message: 'Stock data updated', code: stockCode }), (publishErr, publishReply) => {
-      if (publishErr) {
-        console.error('Error publishing message:', publishErr);
-        return;
-      }
-      console.log('Publish data reply:', publishReply);
-      // 실제 어플리케이션에서는 연결을 유지할 수 있지만, 예제를 위해 연결을 종료합니다.
-      publisher.quit();
-    });
-  });
-  
 
+    // publisher가 key: channelName, value: JSON.stringify({})를 발행한다.
+    publisher.publish(
+      channelName,
+      JSON.stringify({ message: "Stock data updated", code: stockCode }),
+      (publishErr, publishReply) => {
+        if (publishErr) {
+          console.error("Error publishing message:", publishErr);
+          return;
+        }
+        console.log("Publish data reply:", publishReply);
+        // 실제 어플리케이션에서는 연결을 유지할 수 있지만, 예제를 위해 연결을 종료합니다.
+        publisher.quit();
+      }
+    );
+  });
 }
 
-
 module.exports = publish;
-  
+
+/*
+Server: Publisher
+const publisher = redis.createClient({host: ~~, no_ready_check: true})
+
+publisher.publish("products", JSON.stringify(product))
+
+Client: Subscriber
+
+const redis = require('redis')
+const subscriber = redis.createClient({host: ~, no_ready_check: true})
+const products = [];
+
+subscriber.on("message", (channel, message) => {
+  products.push(JSON.parse(message));
+})
+// products라는 key와 그에 대한 값을 기다림
+// 값이 바뀌면 Client에서 받음
+subscriber.subscribe("products");
+*/
