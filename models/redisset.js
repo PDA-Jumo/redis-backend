2930ce8649eac32dc58465923a0ac8e4068f2e49
Youjun Seo <76039485+SEO-YJ@users.noreply.github.com>
2024-03-27 10:31:47 +0900

    Chore: 충돌 변경

--- models/redisset.js
+++ models/redisset.js
@@ -1,36 +1,33 @@
 const publisher = require("./redis");
 
 // 데이터 저장 및 변경 사항 알림
-<<<<<<< HEAD
 function publish(stockCode, stockData) {
-  const channelName = stockCode;
-=======
-function publish(stockCode, stockData){
-  //데이터 저장(set)
->>>>>>> 499d7fb1199083cbcf4620d502a2ce45a5100756
   publisher.set(stockCode, JSON.stringify(stockData), (err, reply) => {
     if (err) {
       console.error("Error setting value in Redis:", err);
       return;
     }
-    console.log('Set data reply:', reply);
+    console.log("Set data reply:", reply);
     // 데이터 변경 사항을 해당 주식 코드의 채널에 알림(publish)
-    publisher.publish(stockCode, JSON.stringify(stockData), (publishErr, publishReply) => {
-      if (publishErr) {
-        console.error('Error publishing message:', publishErr);
-        return;
+    publisher.publish(
+      stockCode,
+      JSON.stringify(stockData),
+      (publishErr, publishReply) => {
+        if (publishErr) {
+          console.error("Error publishing message:", publishErr);
+          return;
+        }
+        console.log("Publish data reply:", publishReply);
+        if (publishReply > 0) {
+          console.log("Message published successfully");
+        } else {
+          console.log("No subscribers for this channel");
+        }
+        //연결을 종료
+        publisher.quit();
       }
-      console.log('Publish data reply:', publishReply);
-      if (publishReply > 0) {
-        console.log('Message published successfully');
-      } else {
-        console.log('No subscribers for this channel');
-      }
-      //연결을 종료
-      publisher.quit();
-    });
+    );
   });
-  
 }
 
 module.exports = publish;
